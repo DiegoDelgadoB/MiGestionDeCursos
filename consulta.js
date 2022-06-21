b751f7edcd2165345066c7fe62dfa7e9f25e05fb
Diego Delgado Barra <diego.delgadobarra@outlook.com>
2022-06-21 01:05:54 -0400

    LastCommit;Finished

--- consulta.js
+++ consulta.js
@@ -7,3 +7,60 @@ const pool = new Pool({
     port: 5432
 });
 
+async function nuevoCurso (nombre, nivel, fecha, duracion) {
+    try {
+        const result = await pool.query(
+            `INSERT INTO cursos (nombre, nivel, fecha, duracion)
+            VALUES ('${nombre}','${nivel}','${fecha}','${duracion}')
+            RETURNING *`
+        );
+        console.log(result)
+        return result.rows;
+            
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+async function prepararCurso() {
+    try{
+        const result = await pool.query(`SELECT * FROM cursos`);
+        return result.rows;
+
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+async function editarCurso (id, nombre, nivel, fecha, duracion){
+    try{
+        const result = await pool.query(`
+            UPDATE cursos SET
+            nombre = '${nombre}',
+            nivel = '${nivel}',
+            fecha = '${fecha}',
+            duracion = '${duracion}'
+            WHERE id = '${id}'
+            RETURNING *
+        `);
+        return result.rows;
+
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+async function eliminarCurso (id) {
+    try {
+        const result = await pool.query(`
+            DELETE FROM cursos WHERE id = '${id}'
+        `);
+        
+        return result.rowCount;
+
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+module.exports = { nuevoCurso, prepararCurso, editarCurso, eliminarCurso };
